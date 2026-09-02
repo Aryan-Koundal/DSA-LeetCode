377bf51fdacc2bd93e7981f9e577702b7b9c479a
Aryan-Koundal <aryankoundal3039@gmail.com>
2026-09-02 22:41:09 +0530

    Time: 136 ms (27.04%), Space: 13 MB (55.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        x=""
+        for i in s:
+            if i.isalnum():
+                x +=i
+        a = x[::-1].lower()
+        x = x.lower()
+        if x == a:
+            return True
+        elif x != a:
+            return False 
+
+        
